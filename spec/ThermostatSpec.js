ef33e4581e50333d070df64e8f64306083bc96e6
shadz22 <shadz22@yahoo.co.uk>
2020-01-22 16:47:53 +0000

    have added up and down functions for temperature

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,24 @@
+'use strict';
+
+describe('Thermostat', function() {
+	var thermostat;
+	
+	beforeEach(function() {
+		thermostat = new Thermostat();
+	});
+
+	it('starts at 20 degrees', function() {
+		expect(thermostat.temperature).toEqual(20);
+	});
+
+	it('increases the temperature', function(){
+		thermostat.up();
+		expect(thermostat.getCurrentTemperature()).toEqual(21)
+	});
+
+	it('decreases the temperature', function() {
+		thermostat.down();
+		expect(thermostat.getCurrentTemperature()).toEqual(19);
+	});
+
+});
