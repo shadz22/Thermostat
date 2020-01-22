8819b070e649cd5803a1e98cfa8915de8920ccde
shadz22 <shadz22@yahoo.co.uk>
2020-01-22 17:23:06 +0000

    add default minimum temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -11,14 +11,21 @@ describe('Thermostat', function() {
 		expect(thermostat.temperature).toEqual(20);
 	});
 
-	it('increases the temperature', function(){
+	it('increases the temperature by 1 degree', function(){
 		thermostat.up();
 		expect(thermostat.getCurrentTemperature()).toEqual(21)
 	});
 
-	it('decreases the temperature', function() {
+	it('decreases the temperature by 1 degree', function() {
 		thermostat.down();
 		expect(thermostat.getCurrentTemperature()).toEqual(19);
+	});
+
+	it('has a default minimum temperature of 10 degrees', function() {
+		for (var i = 0; i < 11; i++ ) {
+			thermostat.down();
+		}
+		expect(thermostat.getCurrentTemperature()).toEqual(10);
 	});
 
 });
